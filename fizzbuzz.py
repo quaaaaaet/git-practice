4e7e3f42bd5bcfc0d0cda0a7479fe2e3d83929c3
yubin sin <tlsdbqls06@naver.com>
2023-05-01 00:04:49 +0900

    feat: Done fizzbuzz
    Print fizzbuzz if i is multiple of 15.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,9 @@ def do_fizzbuzz(num : int):
 
     """
     for i in range(1,num+1):
-        if i%3==0:
+        if i%15==0:
+            print('fizzbuzz')
+        elif i%3==0:
             print('fizz')
         elif 1%5==0:
             print('buzz')
